18083b31001635ba1043d3f1551e84f7cde6d20e
Andrea Cioni <supercionci@hotmail.it>
2023-05-15 23:31:00 +0200

    use npm dependency

--- apps/saml2-nest-poc/src/app.module.ts
+++ apps/saml2-nest-poc/src/app.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { SecurityModule } from 'saml2-nest-lib/lib/security/security.module';
-import { CommonsModule } from 'saml2-nest-lib/lib/commons/commons.module';
-import { UserModule } from 'saml2-nest-lib/lib/security/user/user.module';
+import { SecurityModule } from '@andreacioni/saml2-nest-lib/lib/security/security.module';
+import { CommonsModule } from '@andreacioni/saml2-nest-lib/lib/commons/commons.module';
+import { UserModule } from '@andreacioni/saml2-nest-lib/lib/security/user/user.module';
 
 @Module({
   imports: [SecurityModule, CommonsModule, UserModule],
